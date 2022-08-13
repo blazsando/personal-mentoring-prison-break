52b292dd361cabab383624937377ba9d65d9e3d4
andobalazsandras <balazs.ando@codecool.com>
2022-08-13 09:49:50 +0200

    Add solution

---
+++ main.py
@@ -0,0 +1,11 @@
+from Prisoner import Prison
+
+
+def main(prisoner_num):
+    print(f'rabok szama: {prisoner_num}')
+    prison = Prison(prisoner_num)
+    print(f'szabadulas napja: {prison.prison_break()}')
+
+
+if __name__ == '__main__':
+    main(100)
